6e911827b6614e2e3a4e1473877259eba6768e8b
husseinagrama <husseinagrama@gmail.com>
2017-03-07 21:29:37 -0600

    seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+items = [
+  [ "tablet", "Apple", "iPad", "$499", "", "it's cool now, but will Apple keep its cool?"],
+  [ "tablet", "Dell", "DellPad", "$498", "", "Dell is damn reliable."],
+  [ "tablet", "Amazon", "KindleFire", "$199", "", "Sure it's cheap. Works OK too. But do you really want to be trapped in Bezos' ecosystem?"],
+  [ "tablet" "Microsoft", "$500", "", "Microsoft. What can we say? Can't live with them, can't live without them." ]
+]
+
+items.each do |item|
+  Item.create( :device => item[0], :brand => item[1], :price => item[2], :image => item[3], :description => item[4] )
+end
