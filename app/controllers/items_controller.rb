7afbd42b342154fc46124e02239909fa5deb00ac
husseinagrama <husseinagrama@gmail.com>
2017-03-12 13:39:06 -0500

    added update,delete, edit actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,48 @@
 class ItemsController < ApplicationController
-  def show_items_method
+  def index
     @items = Item.all
-    render "show_items.html.erb"
+    render "index.html.erb"
+  end
+  def new
+    render "new.html.erb"
+  end
+  
+  def create
+    item = Item.new(
+        brand: params[:brand],
+        model: params[:model],
+        price: params[:price],
+        description: params[:description]
+      )
+    item.save
+    render "create.html.erb"
+  end
+
+  def show
+    item_id = params[:id]
+    @item = Item.find_by(id: item_id)
+    render "show.html.erb"
+  end
+  def edit
+    item_id = params[:id]
+    @item = Item.find_by(id: item_id)
+    render "edit.html.erb"
+  end
+  def update
+    item_id = params[:id]
+    @item = Item.find_by(id: item_id)
+    @item.brand = params[:brand]
+    @item.model = params[:model]
+    @item.price = params[:price]
+    @item.description = params[:description]
+    @item.save
+    render "update.html.erb"
+  end
+  def destroy
+    item_id = params[:id]
+    @item = Item.find_by(id: item_id)
+    @item.destroy
+    render "delete.html.erb"
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get "/shop_url" => "items#show_items_method"
+  get "/shop_url" => "items#index"
+  get "/" => "items#index"
+  get "/items/new" => "items#new"
+  post "/items" => "items#create"
+  get "/items/:id/edit" => "items#edit"
+  patch "/items/:id" => "items#update"
+  delete "/items/:id" => "items#destroy"
+
+  get "/items/:id" => "items#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
