96f828b93521b554b6ad51d1afaf200274c3a824
husseinagrama <husseinagrama@gmail.com>
2017-03-15 21:30:24 -0500

    changed data attributes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  def sale_message
+    
+    #if price.to_i < 200
+     # return "Discount item!"
+    #else
+      #return "special Everyday price!"
+    #end
+    
+  end 
+
+  def tax
+    return price.to_f * 0.09
+  end
+  
+  def total
+    return price.to_f + tax
+  end
+  #def price_class_method
+   # if sale_message == "Discount item!"
+     # return "discount-item"
+  #end
+  def discounted?
+    return price.to_f < 200  
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308014249) do
+ActiveRecord::Schema.define(version: 20170316022101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170308014249) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "description"
+    t.text     "description"
   end
 
 end
